14d6c0b3c517e9b793cb936a89b504659e0d21ee
Boyang Hu <i@boyang.im>
2026-09-13 01:05:29 -0700

    i18n: Traditional Chinese, Japanese, Russian, Korean for the console and portal

--- web/admin/src/i18n/index.ts
+++ web/admin/src/i18n/index.ts
@@ -2,15 +2,19 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import zh from './zh-CN.json'
+import zhTW from './zh-TW.json'
 import en from './en.json'
+import ja from './ja.json'
+import ru from './ru.json'
+import ko from './ko.json'
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: { 'zh-CN': { translation: zh }, en: { translation: en } },
+    resources: { 'zh-CN': { translation: zh }, 'zh-TW': { translation: zhTW }, en: { translation: en }, ja: { translation: ja }, ru: { translation: ru }, ko: { translation: ko } },
     fallbackLng: 'zh-CN',
-    supportedLngs: ['zh-CN', 'en'],
+    supportedLngs: ['zh-CN', 'zh-TW', 'en', 'ja', 'ru', 'ko'],
     interpolation: { escapeValue: false },
     detection: { order: ['localStorage', 'navigator'], caches: ['localStorage'] },
   })
